62a1bf4bba62d7a27df052757ff0b76701d4b77a
mat557 <71515771+mat557@users.noreply.github.com>
2023-11-16 01:52:39 -0800

    Step to auth

---
+++ src/pages/components/AddUser/EditManagement.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const EditManagement = () => {
+    let content
+
+    
+    return content
+}
+
+export default EditManagement
---
+++ src/pages/components/AddUser/WebManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WebManagement = () => {
+    let content
+
+    return content
+}
+
+export default WebManagement
