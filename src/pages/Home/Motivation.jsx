178f482a4f42f04ea23eded659ea151ab49f466c
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-11-19 02:57:05 +0600

    mission and vission

--- src/pages/Home/Motivation.jsx
+++ src/pages/Home/Motivation.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import img1 from "../../assets/mission/img1.png";
+import mission from "../../assets/mission.jpg";
+import value from "../../assets/value.jpg";
+import collaboration from "../../assets/collaboration.jpg";
+import vision from "../../assets/vision.png";
 
 const Motivation = () => {
   return (
     <div>
       <div className="text-center py-5 mt-10">
-        <h1 className="sm:text-3xl text-3xl  border-y-4 lg:w-1/3 sm:w-2/3 mx-auto py-3 font-medium title-font mb-4 text-gray-900">
+        <h1 className="sm:text-3xl text-3xl  border-y-4 w-1/3 mx-auto py-3 font-medium title-font mb-4 text-gray-900">
           Our Mission & Vission
         </h1>
       </div>
@@ -16,12 +19,12 @@ const Motivation = () => {
               <div className="relative flex flex-col items-start m-1 transition duration-400 ease-in-out delay-150 transform bg-white  shadow-xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
                 <img
                   className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
-                  src={img1}
+                  src={vision}
                   alt="blog"
                 />
                 <div className="px-6 py-8">
                   <h4 className="mt-4 text-2xl font-semibold text-neutral-600">
-                    <span className="">Entry</span>
+                    <span className="">Vision</span>
                   </h4>
                   <p className="mt-4 text-base font-normal text-gray-500 leading-relax">
                     Install Tailwind CSS without any Javascript Framewrok
@@ -37,12 +40,12 @@ const Motivation = () => {
               <div className="relative flex flex-col items-start m-1 transition duration-400 ease-in-out delay-150 transform bg-white  shadow-xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
                 <img
                   className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
-                  src={img1}
+                  src={mission}
                   alt="blog"
                 />
                 <div className="px-6 py-8">
                   <h4 className="mt-4 text-2xl font-semibold text-neutral-600">
-                    <span className="">Entry</span>
+                    <span className="">Mission</span>
                   </h4>
                   <p className="mt-4 text-base font-normal text-gray-500 leading-relax">
                     Install Tailwind CSS without any Javascript Framewrok
@@ -59,12 +62,12 @@ const Motivation = () => {
               <div className="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white  shadow-xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
                 <img
                   className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
-                  src={img1}
+                  src={value}
                   alt="blog"
                 />
                 <div className="px-6 py-8">
                   <h4 className="mt-4 text-2xl font-semibold text-neutral-600">
-                    <span className="">Entry</span>
+                    <span className="">Value</span>
                   </h4>
                   <p className="mt-4 text-base font-normal text-gray-500 leading-relax">
                     Install Tailwind CSS without any Javascript Framewrok
@@ -80,12 +83,12 @@ const Motivation = () => {
               <div className="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white  shadow-xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
                 <img
                   className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
-                  src={img1}
+                  src={collaboration}
                   alt="blog"
                 />
                 <div className="px-6 py-8">
                   <h4 className="mt-4 text-2xl font-semibold text-neutral-600">
-                    <span className="">Entry</span>
+                    <span className="">Collaboration</span>
                   </h4>
                   <p className="mt-4 text-base font-normal text-gray-500 leading-relax">
                     Install Tailwind CSS without any Javascript Framewrok
