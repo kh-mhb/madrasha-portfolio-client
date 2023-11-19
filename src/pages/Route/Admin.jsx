4907158ba532a39c9bbbc36bd7ab3248bd42f0e7
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-11-19 07:00:57 +0600

    gallery admin dashboard footer

--- src/pages/Route/Admin.jsx
+++ src/pages/Route/Admin.jsx
@@ -4,8 +4,6 @@ import Footer from "../Footer/Footer";
 import { Link, Outlet, useLocation } from "react-router-dom";
 
 const Admin = () => {
-  
-
   return (
     <div>
       <Header></Header>
@@ -13,11 +11,12 @@ const Admin = () => {
       <div className="py-5">
         <div className="drawer">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content">
+          <div className="drawer-content text-center">
             <Outlet></Outlet>
             <label
               htmlFor="my-drawer"
-              className="btn bg-blue-600 text-white fixed bottom-16 btn-sm drawer-button ml-1">
+              className="btn bg-blue-600 text-white lg:fixed top-10 right-20 btn-sm drawer-button ml-1"
+            >
               Dashboard
             </label>
           </div>
@@ -38,16 +37,42 @@ const Admin = () => {
           </div>
         </div>
       </div>
+      <div>
+        {/* new section */}
+        <section>
+          <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
+            <div className="flex flex-col w-full mb-12 text-center">
+              <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
+                Lutfia Atimkhana
+              </h1>
+              <p className="max-w-xl mx-auto mt-8 text-base leading-relaxed text-center text-gray-500">
+                The majority of our work can be divided up into what we call our
+                6 pillar.Each one a seperate field that supports of or lifts up
+                our organization as a whole
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
 
-      <Footer></Footer>
+      <div className="w-3/5 mx-auto">
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
 
 export default Admin;
 
-
-                {/* <Link to="/adminLayout/addstudent">Add Student</Link> */}
-                {/* <Link to="/adminLayout/addcommitte">Add Committe</Link> */}
-                {/* <Link to="/adminLayout/addteacher">Add Teacher</Link> */}
-                {/* <Link to="/adminLayout/addeditorials">Add Editorials</Link> */}
+{
+  /* <Link to="/adminLayout/addstudent">Add Student</Link> */
+}
+{
+  /* <Link to="/adminLayout/addcommitte">Add Committe</Link> */
+}
+{
+  /* <Link to="/adminLayout/addteacher">Add Teacher</Link> */
+}
+{
+  /* <Link to="/adminLayout/addeditorials">Add Editorials</Link> */
+}
