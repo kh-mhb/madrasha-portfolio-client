d7f4295e843692345213cfa6f41dcc75270fabb6
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-11-23 20:34:03 +0600

    gallery added

--- src/pages/components/Gallery/Gallery.jsx
+++ src/pages/components/Gallery/Gallery.jsx
@@ -1,50 +1,66 @@
-import React from 'react';
-import LightGallery from 'lightgallery/react';
-import img1 from '../../../assets/pillar/img1.jpeg';
-import img2 from '../../../assets/pillar/img2.jpeg';
-import img3 from '../../../assets/pillar/img3.jpeg';
-import img4 from '../../../assets/pillar/img4.jpeg';
-import 'lightgallery/css/lightgallery.css';
-import 'lightgallery/css/lg-zoom.css';
-import 'lightgallery/css/lg-thumbnail.css';
-import 'lightgallery/css/lg-autoplay.css';
-import 'lightgallery/css/lg-fullscreen.css';
-import 'lightgallery/css/lg-share.css';
-import 'lightgallery/css/lg-rotate.css';
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
-import lgZoom from 'lightgallery/plugins/zoom';
-import lgAutoplay from 'lightgallery/plugins/autoplay';
-import lgFullscreen from 'lightgallery/plugins/fullscreen';
-import lgShare from 'lightgallery/plugins/share';
-import lgRotate from 'lightgallery/plugins/rotate';
+import React from "react";
 
+import LightGallery from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import img1 from "../../../assets/pillar/img1.jpeg";
+import img2 from "../../../assets/pillar/img2.jpeg";
+import img3 from "../../../assets/pillar/img3.jpeg";
+import img4 from "../../../assets/pillar/img4.jpeg";
 
-const images = [
-  { src: img1, alt: 'Flag of India' },
-  { src: img2, alt: '2' },
-  { src: img3, alt: '3' },
-  { src: img4, alt: '4' },
-]
+class ErrorBoundary extends React.Component {
+  componentDidCatch() {}
 
-
-export function Gallery() {
-  const onInit = () => {
-
+  render() {
+    return this.props.children;
   }
-
- 
-  return (
-    <div  className="container w-full flex flex-wrap my-8">
-      <LightGallery
-        onInit={onInit}
-        speed={500}
-        plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate, lgShare]}>
-        {images.map((image, index) => (
-          <a className='w-1/3' href={image.src} key={index}>
-            <img alt={image.alt} className="w-1/3 h-auto"  src={image.src} />
-          </a>
-        ))}
-      </LightGallery>
-    </div>
-  )
 }
+
+const Gallery = () => {
+  const onInit = () => {};
+
+  return (
+    <div>
+      <div className="text-center mt-5 ">
+        <h1 className="sm:text-3xl text-3xl  border-y-4 w-1/3 mx-auto py-3 font-medium title-font mb-4 text-gray-900">
+          From Our Gallery
+        </h1>
+      </div>
+      <ErrorBoundary>
+        <div className="container mx-auto mt-6">
+          <LightGallery
+            onInit={onInit}
+            speed={500}
+            plugins={[lgThumbnail, lgZoom]}
+          >
+            <a className="mb-3" href={img1} data-lg-size="1600-1067">
+              <img alt="img1" src={img1} className="w-full h-full" />
+            </a>
+            <a href={img2} data-lg-size="1600-1067">
+              <img alt="img2" src={img2} className="w-full h-auto" />
+            </a>
+            <a href={img3} data-lg-size="1600-1067">
+              <img alt="img3" src={img3} className="w-full h-auto" />
+            </a>
+            <a href={img4} data-lg-size="1600-1067">
+              <img alt="img4" src={img4} className="w-full h-auto" />
+            </a>
+          </LightGallery>
+        </div>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 mx-auto flex flex-wrap">
+            <div className="flex w-full mb-20 flex-wrap"></div>
+            <div className="flex flex-wrap md:-m-2 -m-1">
+              {/* Rest of your code for the image gallery */}
+            </div>
+          </div>
+        </section>
+      </ErrorBoundary>
+    </div>
+  );
+};
+
+export default Gallery;
