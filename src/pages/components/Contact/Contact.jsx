040d595615b87c6af469797e1255858a94da2142
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-11-26 21:35:04 +0600

    contact new added

--- src/pages/components/Contact/Contact.jsx
+++ src/pages/components/Contact/Contact.jsx
@@ -60,12 +60,10 @@ const Contact = () => {
             </div>
           </div>
           <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+            <h2 className="text-gray-900 text-2xl mb-1 font-medium title-font">
               Feedback
             </h2>
-            <p className="leading-relaxed mb-5 text-gray-600">
-              Post-ironic portland shabby chic echo park, banjo fashion axe
-            </p>
+
             <div className="relative mb-4">
               <label htmlFor="name" className="leading-7 text-sm text-gray-600">
                 Name
@@ -107,10 +105,6 @@ const Contact = () => {
             <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Button
             </button>
-            <p className="text-xs text-gray-500 mt-3">
-              Chicharrones blog helvetica normcore iceland tousled brook viral
-              artisan.
-            </p>
           </div>
         </div>
       </section>
